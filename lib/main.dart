5a390844cd68a638de443ed8933ed5a3788143a3
LEEHYUNDONG <l.steve10@yahoo.com>
2023-05-30 17:17:16 +0900

    button 추가

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Item Order Service'),
+      home: const MyHomePage(title: '상품 목록'),
     );
   }
 }
@@ -44,68 +44,57 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: FutureBuilder<List<Item>>(
-        future: fetchItem(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            // print(snapshot.data!.first.itemName);
-            return ListView.builder(
-                itemCount: snapshot.data?.length,
-                itemBuilder: (context, index) {
-                  // Item item = snapshot.data!.elementAt(index);
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: SafeArea(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: FutureBuilder(
+                  future: fetchItem(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      // print(snapshot.data!.first.itemName);
+                      return ListView.builder(
+                          itemCount: snapshot.data?.length,
+                          itemBuilder: (context, index) {
+                            // Item item = snapshot.data!.elementAt(index);
 
-                  return Card(
-                    child: Text('$snapshot.data[index]'),
-                  );
-                });
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        },
-      )),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
-    );
-    //     return Scaffold(
-    //   appBar: AppBar(
-    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //       child: FutureBuilder<List<Item>>(
-    //     future: itemLst,
-    //     builder: (context, snapshot) {
-    //       if (snapshot.hasData) {
-    //         print(snapshot.data!.first.itemName);
-    //         return ListView.builder(
-    //             itemCount: snapshot.data?.length,
-    //             itemBuilder: (context, index) {
-    //               // Item item = snapshot.data!.elementAt(index);
-
-    //               return Card(
-    //                 child: Text('$itemLst[index]'),
-    //               );
-    //             });
-    //       } else if (snapshot.hasError) {
-    //         return Text("${snapshot.error}");
-    //       }
-    //       return CircularProgressIndicator();
-    //     },
-    //   )),
-    //   // floatingActionButton: FloatingActionButton(
-    //   //   onPressed: _incrementCounter,
-    //   //   tooltip: 'Increment',
-    //   //   child: const Icon(Icons.add),
-    //   // ),
-    // );
+                            return Card(
+                              child: Text('$snapshot.data[index]'),
+                            );
+                          });
+                    } else if (snapshot.hasError) {
+                      return Text("${snapshot.error}");
+                    }
+                    return CircularProgressIndicator();
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OutlinedButton(
+                    onPressed: () => _updateItem(),
+                    child: Text("상품수정"),
+                  ),
+                  OutlinedButton(
+                    onPressed: () => _addItem(),
+                    child: Text("상품등록"),
+                  )
+                ],
+              )
+            ],
+          )),
+        ));
   }
 }
+
+_addItem() {}
+
+_updateItem() {}
